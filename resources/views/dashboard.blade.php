b2892a7c1cca67664f06fa03ab14e85e45ba29b2
Hessan-harb <hessanmohammedhassan@gmail.com>
2023-12-12 20:44:55 +0200

    Livewire

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight ">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                <span><img src="https://picsum.photos/id/238/1000/200" alt="" ></span> <br>   
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo eveniet iste est, velit veniam incidunt nobis rerum quia, at eum dolor quod? A voluptatum dolor blanditiis. Incidunt amet, perspiciatis numquam aspernatur error, commodi est tenetur accusamus sunt alias reprehenderit modi magni ullam exercitationem culpa consequuntur itaque. Unde, placeat sit. Ullam inventore natus mollitia aperiam aliquid doloribus eos, accusamus iure. Amet molestias laudantium dolores necessitatibus sunt eum deserunt est nesciunt at voluptate, nulla tempore aspernatur harum eveniet pariatur consectetur sit nihil consequuntur. Debitis fugit molestias iusto, deleniti vitae quibusdam quasi a qui ratione eveniet rem maxime dignissimos quos sed tempore ea eum! Tenetur odio eum vero dolores iure asperiores unde provident! Fugiat ducimus explicabo ipsam? Nemo quae in explicabo et natus perferendis esse inventore dolor maxime cum, facere reprehenderit! Quae fugit neque, consequuntur ipsum iure deserunt facilis rem quis fuga deleniti magnam temporibus aliquam repellendus dolore cupiditate velit sint. Id voluptate tempore sed molestias accusantium maiores labore commodi doloremque repudiandae. Error accusantium laudantium nam incidunt minus tenetur hic alias dolorem vitae? Nulla amet enim ad velit officiis architecto ipsa vitae rerum id fugiat non, eos optio at minima quod! Vero officia iste maiores aliquam laborum non. Ut repellendus reprehenderit provident id.
+                <br><span><img src="https://picsum.photos/id/200/1000/200" alt="" ></span> <br> 
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo eveniet iste est, velit veniam incidunt nobis rerum quia, at eum dolor quod? A voluptatum dolor blanditiis. Incidunt amet, perspiciatis numquam aspernatur error, commodi est tenetur accusamus sunt alias reprehenderit modi magni ullam exercitationem culpa consequuntur itaque. Unde, placeat sit. Ullam inventore natus mollitia aperiam aliquid doloribus eos, accusamus iure. Amet molestias laudantium dolores necessitatibus sunt eum deserunt est nesciunt at voluptate, nulla tempore aspernatur harum eveniet pariatur consectetur sit nihil consequuntur. Debitis fugit molestias iusto, deleniti vitae quibusdam quasi a qui ratione eveniet rem maxime dignissimos quos sed tempore ea eum! Tenetur odio eum vero dolores iure asperiores unde provident! Fugiat ducimus explicabo ipsam? Nemo quae in explicabo et natus perferendis esse inventore dolor maxime cum, facere reprehenderit! Quae fugit neque, consequuntur ipsum iure deserunt facilis rem quis fuga deleniti magnam temporibus aliquam repellendus dolore cupiditate velit sint. Id voluptate tempore sed molestias accusantium maiores labore commodi doloremque repudiandae. Error accusantium laudantium nam incidunt minus tenetur hic alias dolorem vitae? Nulla amet enim ad velit officiis architecto ipsa vitae rerum id fugiat non, eos optio at minima quod! Vero officia iste maiores aliquam laborum non. Ut repellendus reprehenderit provident id.
+  
+
+            </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
